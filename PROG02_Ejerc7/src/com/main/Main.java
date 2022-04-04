e92d32a0fad47087c2a974d1340e10480274b010
sergio <sergio.vegas.alonso@gmail.com>
2022-04-04 19:16:05 +0200

    Tema 2, ejercicio 7.

---
+++ PROG02_Ejerc7/src/com/main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+public class Main {
+    public static void main(String[] args) {
+        float c1 = 4.52f;
+        float c2 = 8f;
+        float result = (c2 / c1) * -1;
+
+        System.out.printf("%.4fx + %.4f = 0, x = %.4f", c1, c2, result);
+    }
+}
